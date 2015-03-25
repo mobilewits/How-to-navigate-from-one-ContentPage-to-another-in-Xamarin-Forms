e650d93883c279027b679ebc9d29f87d67707e16
Mobilewits Mobile apps developers <harry@Harrys-Mac-Pro.local>
2015-03-25 13:27:52 -0400

    Droid iOS NavigationSegueXamarinForms NavigationSegueXamarinForms.sln NavigationSegueXamarinForms.userprefs packages

---
+++ NavigationSegueXamarinForms/MainView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace NavigationSegueXamarinForms
+{
+	public class MainView : ContentPage
+	{
+		public MainView ()
+		{
+			var btn_PushAsync = new Button {
+				Text = "PushAsync"
+			};
+
+			btn_PushAsync.Clicked+= async (sender, e) => {
+				await Navigation.PushAsync(new SecondPage());
+			};
+
+
+			Content = new StackLayout { 
+				Children = {
+					btn_PushAsync
+
+				}
+			};
+			Title = "Navigation Demo";
+		}
+	}
+}
+
+
---
+++ NavigationSegueXamarinForms/SecondPage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace NavigationSegueXamarinForms
+{
+	public class SecondPage : ContentPage
+	{
+		public SecondPage ()
+		{
+			var btn_PopAsync = new Button {
+				Text = "PopAsync"
+			};
+			var btn_PushModalAsync = new Button {
+				Text = "PushModalAsync"
+			};
+			var btn_PopModalAsync = new Button {
+				Text = "PopModalAsync"
+			};
+			var btn_PopToRootAsync = new Button {
+				Text = "PopToRootAsync"
+			};
+
+			btn_PopAsync.Clicked+= async (sender, e) => {
+				await Navigation.PopAsync();
+			};
+			btn_PushModalAsync.Clicked+= async (sender, e) => {
+				await Navigation.PushModalAsync(new MainView());
+			};
+			btn_PopModalAsync.Clicked+= async (sender, e) => {
+				await Navigation.PopToRootAsync();
+			};
+			btn_PopToRootAsync.Clicked+= async (sender, e) => {
+				await Navigation.PopToRootAsync();
+			};
+			Content = new StackLayout { 
+				Children = {
+					btn_PopAsync,
+					btn_PushModalAsync,
+					btn_PopModalAsync,
+					btn_PopToRootAsync
+				}
+
+			};
+			Title = "Second Page";
+		}
+	}
+}
+
+
